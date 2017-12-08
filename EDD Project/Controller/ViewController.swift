f098f7f748424654bc3a1f2632af12faceda157a
Joseph Kirven <josephkirven@js-macbook-air.local>
2017-12-07 21:53:38 -0800

    First Commit since creation. glen still hasn't done anything

--- EDD Project/Controller/ViewController.swift
+++ EDD Project/Controller/ViewController.swift
@@ -12,14 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        UNService.shared.authorize()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTimerTapped() {
+        print("timer")
     }
 
+    @IBAction func onDateTapped() {
+        print("date")
+    }
+    
+    @IBAction func onLocationTapped() {
+        print("location")
+    }
+    
 
 }
 
