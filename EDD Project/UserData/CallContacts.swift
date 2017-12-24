8ac7797f667099e81dbf83a73dd6d2580108ad8d
JD <josephkirven@Js-MacBook-Air.local>
2017-12-23 18:55:50 -0800

    Christmas commit
    Some improved app design
    Tried to import firebase SDKs through cocoapods - Not working

---
+++ EDD Project/UserData/CallContacts.swift
@@ -0,0 +1,17 @@
+//
+//  CallContacts.swift
+//  EDD Project
+//
+//  Created by Joseph Kirven on 12/9/17.
+//  Copyright © 2017 NKN Engineering. All rights reserved.
+//
+
+import Foundation
+var ECPrimary1 = String.self;
+var ECPrimary2 = String.self;
+var ECPrimary3 = String.self;
+var ECSecondary1 = String.self;
+var ECSecondary2 = String.self;
+var ECSecondary3 = String.self;
+
+
---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :iOS, ‘7.0’
+pod 'FirebaseDynamicLinks', '~> 2.3'
+pod 'SimpleGoogleAnalyticsForIOS', '~> 0.0'
