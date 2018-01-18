6e4e6eb7efe135922974ebd22cf9b8aabdce83ef
JD <jdkwyo@gmail.com>
2018-01-18 07:52:40 -0800

    Added SQLite

--- EDD Project/Controller/ViewController.swift
+++ EDD Project/Controller/ViewController.swift
@@ -7,26 +7,76 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        UNService.shared.authorize()
+        
+        do {
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let fileUrl = documentDirectory.appendingPathComponent(user)
+        } catch {
+            print(error)
+        }
+        
+         let documentDirectory = FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        
+        
     }
 
-    @IBAction func onTimerTapped() {
-        print("timer")
+    @IBAction func createTable() {
+        print("CREATE TAPPED")
     }
 
-    @IBAction func onDateTapped() {
-        print("date")
+    @IBAction func insertUser() {
+        print("INSERT TAPPED")
+        let alert = UIAlertController(title:"Insert User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder="Name" }
+        alert.addTextField { (tf) in tf.placeholder="Email" }
+        let action = UIAlertAction(title: "submit", style: .default) { (_) in
+            guard let name = alert.textFields?.first?.text,
+                let email = alert.textFields?.last?.text
+                else { return }
+            print(name)
+            print(email)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func onLocationTapped() {
-        print("location")
+    @IBAction func listUsers() {
+        print("LIST TAPPED")
     }
     
-
+    @IBAction func updateUser() {
+        print("UPDATE TAPPED")
+        let alert = UIAlertController(title: "Updated User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder="User ID" }
+        alert.addTextField { (tf) in tf.placeholder="Email" }
+        let action = UIAlertAction(title: "submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text,
+                let email = alert.textFields?.last?.text
+                else { return }
+            print(userIdString)
+            print(email)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func deleteUser() {
+        print("DELETE TAPPED")
+        let alert = UIAlertController(title: "Delete User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder="User ID" }
+        let action = UIAlertAction(title: "submit", style: .default) { (_) in
+            guard let userIdString = alert.textFields?.first?.text else { return }
+            print(userIdString)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
- 
+
